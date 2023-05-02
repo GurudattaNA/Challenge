a797aabc66eee69abdf0d80fcfc8398146074e73
GurudattaNA <gurudatta.na@gmail.com>
2023-05-02 13:35:07 +0530

    Create Challenge2.ps1

---
+++ Challenge2.ps1
@@ -0,0 +1,2 @@
+# Get meta data of an instance within Azure and provide a json formatted output.
+Invoke-RestMethod -Headers @{"Metadata"="true"} -Method GET -NoProxy -Uri "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | ConvertTo-Json -Depth 64
